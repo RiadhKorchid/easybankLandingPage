aac153cb1d95d6f4fb1989e662e831ec5c7f93a2
Riadh Korchid <riadhkourchid@gmail.com>
2022-11-20 00:56:33 +0100

    make the site responsive

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -4,35 +4,36 @@ import restaurant from "../images/image-restaurant.jpg"
 import plane from "../images/image-plane.jpg"
 import confetti from "../images/image-confetti.jpg"
 
-export default function(){
+export default function () {
     return (
         <section className="article-section">
-            <h1>Latest Articles</h1>
+            <div className="header"><h1>Latest Articles</h1></div>
             <div className="articles">
-            <div className="article">
-                <img src={currency} alt="" />
-                <div className="author">By Claire Robinson</div>
-                <h1>Recieve money in any currency with no fees</h1>
-                <p>The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single …</p>
-            </div>
-            <div className="article">
-                <img src={restaurant} alt="" />
-                <div className="author">by Wilson Hutton</div>
-                <h1>Treat yourself without worrying about money</h1>
-                <p>Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you …</p>
-            </div>
-            <div className="article">
-                  <img src={plane} alt="" />
-                <div className="author">By Wilson Hutton</div>
-                <h1>Take your Easybank card wherever you go We want</h1>
-                <p>We want you to enjoy your travels. This is why we don’t charge any fees on purchases while you’re abroad. We’ll even show you ...</p>
-            </div>
-            <div className="article">
-                <img src={confetti} alt="" />
-                <div className="author">By Claire Robinson</div>
-                <h1>Our invite-only Beta accounts are now live!</h1>
-                <p>After a lot of hard work by the whole team, we’re excited to launch our closed beta. It’s easy to request an invite through the site ...</p>
-            </div>
+                <div className="article">
+                    <img src={currency} alt="" />
+                    <div className="author">By Claire Robinson</div>
+                    <h1>Recieve money in any currency with no fees</h1>
+                    <p>The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single …</p>
+                </div>
+                <div className="article">
+                    <img src={restaurant} alt="" />
+                    <div className="author">by Wilson Hutton</div>
+                    <h1>Treat yourself without worrying about money</h1>
+                    <p>Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you …</p>
+                </div>
+                <div className="article">
+                    <img src={plane} alt="" />
+                    <div className="author">By Wilson Hutton</div>
+                    <h1>Take your Easybank card wherever you go We want</h1>
+                    <p>We want you to enjoy your travels. This is why we don’t charge any fees on purchases while you’re abroad. We’ll even show you ...</p>
+                </div>
+                <div className="article">
+                    <img src={confetti} alt="" />
+                    <div className="author">By Claire Robinson</div>
+                    <h1>Our invite-only Beta accounts are now live!</h1>
+                    <p>After a lot of hard work by the whole team, we’re excited to launch our closed beta. It’s easy to request an invite through the site ...</p>
+                </div>
             </div>
         </section>
-    )}
+    )
+}
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,38 +1,36 @@
 import React from "react";
 import logo from "../images/logo.svg"
 import hamburger from "../images/icon-hamburger.svg"
-export default function (){
-    function show (){
+export default function () {
+    function show() {
         document.querySelector(".image").classList.toggle("clicked");
         document.querySelector(".mobile-list").classList.toggle("show");
     }
-    return(
+    return (
         <nav>
-            <div className="left">
-            <img src={logo} alt="" />                
-            </div>
+            <img src={logo} alt="" />
             <div className="center">
-            <ul className="desktop-list">
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-                <li>Blog</li>
-                <li>Careers</li>
-             </ul>
+                <ul className="desktop-list">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Blog</li>
+                    <li>Careers</li>
+                </ul>
 
             </div>
-                <div className="right">
-                    <div className="image" onClick={show}></div>
-                    <button>Request Invite</button>
-                    </div>
-            
-                    <ul className="mobile-list">
+            <div className="right">
+                <div className="image" onClick={show}></div>
+                <button>Request Invite</button>
+            </div>
+
+           <div className="div"> <ul className="mobile-list">
                 <li>Home</li>
                 <li>About</li>
                 <li>Contact</li>
                 <li>Blog</li>
                 <li>Careers</li>
-             </ul>
+            </ul></div>
         </nav>
-     )
+    )
 }
