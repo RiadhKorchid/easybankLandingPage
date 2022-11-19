76e1915345745d03e505c5c732ccccde0c081b4c
Riadh Korchid <riadhkourchid@gmail.com>
2022-11-19 22:30:27 +0100

    make the project responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,22 +1,38 @@
 import React from "react";
 import logo from "../images/logo.svg"
+import hamburger from "../images/icon-hamburger.svg"
 export default function (){
+    function show (){
+        document.querySelector(".image").classList.toggle("clicked");
+        document.querySelector(".mobile-list").classList.toggle("show");
+    }
     return(
         <nav>
             <div className="left">
             <img src={logo} alt="" />                
             </div>
             <div className="center">
-             <ul>
+            <ul className="desktop-list">
                 <li>Home</li>
                 <li>About</li>
                 <li>Contact</li>
                 <li>Blog</li>
                 <li>Careers</li>
              </ul>
+
             </div>
-                <button>Request Invite</button>
-       
+                <div className="right">
+                    <div className="image" onClick={show}></div>
+                    <button>Request Invite</button>
+                    </div>
+            
+                    <ul className="mobile-list">
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+                <li>Blog</li>
+                <li>Careers</li>
+             </ul>
         </nav>
      )
 }
