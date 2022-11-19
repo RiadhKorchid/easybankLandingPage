b5f8d5317d439dd1975a06f399655b3b05e857bc
Riadh Korchid <riadhkourchid@gmail.com>
2022-11-19 15:50:50 +0100

    create footer and article

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import "../styles/app.scss"
 import Nav from "./Nav"
 import Home from "./Home"
+import Article from "./Article"
+import Footer from"./Footer"
 export default function () {
     return (<>
         <Nav />
         <Home/>
+        <Article/>
+        <Footer/>
         </>
     )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import logo from "../images/logo.svg"
+import { FaFacebook } from "react-icons/fa"
+import { FaYoutube } from "react-icons/fa"
+import { FaTwitter } from "react-icons/fa"
+import { FaPinterest } from "react-icons/fa"
+import { FaInstagram } from "react-icons/fa"
+export default function () {
+    return (
+        <footer>
+            <div className="footer-left">
+                <img src={logo} className="logo" />
+                <div className="icons">
+                    <FaFacebook className="icon" />
+                    <FaYoutube className="icon" />
+                    <FaTwitter className="icon" />
+                    <FaPinterest className="icon" />
+                    <FaInstagram className="icon" />
+                </div>
+            </div>
+            <div className="footer-center">
+                <ul>
+                    <li>About us</li>
+                    <li> Contact</li>
+                    <li> Blog</li>
+                    <li> Careers</li>
+                    <li> Support</li>
+                    <li> Privacy Policy</li>
+                </ul>
+            </div>
+            <div className="footer-right">
+                <button>Request Invite</button>
+                <p>&copy; Easybank. All Rights Reserved</p>
+            </div>
+
+        </footer>
+    )
+}
